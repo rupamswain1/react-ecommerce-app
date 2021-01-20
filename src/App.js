3d7f04f9c9d48d257a7ba5291b985a0a38bf3fc3
rupamswain1 <rupamswain1@gmail.com>
2021-01-21 02:06:08 +0530

    react reouter intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/hompage.component';
+import {Route, Link} from 'react-router-dom';
+
+const ProductPage=(props)=>{
+  console.log('Hii');
+  return(
+    <div>
+    <Link to='/productPage/:product'>Product</Link>
+    <button onClick={()=>props.history.push('/')}>HomePage</button>
+    <h1>Product Page</h1>
+    </div>
+    );
+  };
+
+const Product=(props)=>{
+  console.log(props);
+  return(
+    
+    <h1>{props.match.params.product}</h1>);
+  };
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+     
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/productPage' component={ProductPage}/>
+      <Route path='/productPage/:product' component={Product}/>
     </div>
   );
 }
