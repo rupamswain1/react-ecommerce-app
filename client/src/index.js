28eda8dbf59234b05f1cf2c7a3adb6b91a50efd0
Rupam <rupam@pop-os.localdomain>
2021-03-29 16:45:12 +0530

    updated pwa in package.json

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react';
-import * as serviceWorker from '.service-worker';
+import * as serviceWorker from './serviceWorkerRegistration';
 import {store,persistor} from './redux/store';
 
 ReactDOM.render(
